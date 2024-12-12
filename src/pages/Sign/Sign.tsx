2dfe6d82ade69776b10c159c0ff2f98e5038af7b
VolodymyrSvereniak <volodymyr.svereniak@gmail.com>
2024-12-12 18:22:18 +0200

    Merging

--- src/pages/Sign/Sign.tsx
+++ src/pages/Sign/Sign.tsx
@@ -1,44 +1,26 @@
-// import { useState } from 'react';
-// import '@/styles/global/Sign.css';
-// import RightImages from '../../components/Auth/RightImages.jsx';
-// import SignInSignUp from './SignInSignUp.jsx';
-// import SvgCircle from '../../components/SvgCircle.jsx';
+import '@/styles/global/Sign.css';
+import RightImages from '@/components/RightImages.js';
+import SignInSignUp from './SignInSignUp.jsx';
+import SvgCircle from '../../components/SvgCircle.jsx';
 import { useAppSelector } from '@/hooks/useAppSelector';
 
 function Sign() {
   const isSign = useAppSelector((state) => state.sign.isSign);
   console.log(isSign);
-  // const [signCheck, setSignCheck] = useState('in');
-  // function handleSignCheck(signStatus: string): void {
-  //   setSignCheck(value);
-  // }
-
-  // const slideCircleClass = 'up';
 
   return (
     <>
-      {/* <main>
+      <main>
         <div id="sign">
-          {(signCheck === 'in' && (
-            <SignInSignUp page={signCheck} handleSignCheck={handleSignCheck} />
-          )) ||
-            (signCheck === 'up' && (
-              <SignInSignUp
-                page={signCheck}
-                handleSignCheck={handleSignCheck}
-              />
-            )) ||
-            (signCheck === 'reset' && (
-              <SvgCircle page={signCheck} handleSignCheck={handleSignCheck} />
-            ))}
+          {isSign === 'in' || isSign === 'up' ? (
+            <SignInSignUp />
+          ) : (
+            isSign === 'reset' && <SvgCircle />
+          )}
         </div>
 
-        {signCheck === 'reset' ? (
-          <RightImages page={signCheck} svgClass={slideCircleClass} />
-        ) : (
-          <RightImages page={signCheck} />
-        )}
-      </main> */}
+        {isSign === 'reset' && <RightImages />}
+      </main>
     </>
   );
 }
